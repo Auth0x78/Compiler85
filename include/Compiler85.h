959b4083a2fa73b320b259dbda70fe968ce3c77b
Auth0x78 <auth0x78+github@gmail.com>
2025-09-17 00:34:11 +0530

    Initial commit

---
+++ include/Compiler85.h
@@ -0,0 +1,10 @@
+﻿// Compiler85.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <Logger.h>
+#include <asm_lexer.h>
+#include <asm_parser.h>
+#include <fstream>
+#include <iostream>
